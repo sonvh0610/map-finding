abf122462db22250729528b684b237357ff75e21
user137983 <user137983@as096.local>
2018-05-22 12:54:31 -0700

    Fix bug

--- MapFinding/ViewController.swift
+++ MapFinding/ViewController.swift
@@ -200,7 +200,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
             }
                 
         }
-        
+        self.directionButton.isHidden = true
  
     }
 }
